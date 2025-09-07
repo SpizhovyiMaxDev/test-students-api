3f2c108e44e6580cbf78ebdad670b1487ce744f2
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2025-09-07 15:41:30 -0700

    Sample - Initial commit

--- src/main/java/code_env/controllers/StudentController.java
+++ src/main/java/code_env/controllers/StudentController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,8 +27,4 @@ public class StudentController {
         return ResponseEntity.ok(service.getStudents());
     }
     
-    @PostMapping("/students/init")
-    public ResponseEntity<?> initData(){
-        return ResponseEntity.ok(service.initSampleData());
-    }
 }
